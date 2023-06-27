01a4f21f1403341e1ba4b6b9e049efa1160383ac
Eddie Hickey <erhickey@gmail.com>
2023-06-27 16:36:39 -0400

    termion

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-use std::io::{stdout, Write, Stdout};
+use std::io::{stdin, stdout, Result, Stdout, Write};
 
-use futures::StreamExt;
+use termion::{clear, cursor, raw::IntoRawMode, style};
+use termion::color::{self, Bg, Fg};
+use termion::event::Key;
+use termion::input::TermRead;
 
-use crossterm::{
-    cursor, execute, queue, Result,
-    event::{Event, EventStream, KeyCode},
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal::{self, enable_raw_mode, disable_raw_mode}
-};
+const UP: char = '▲';
+const DOWN: char = '▼';
+
+enum ProcessStatus {
+    Running = 1,
+    Halting = 2,
+    Halted = 3
+}
+
+struct Process {
+    label: String,
+    command: String,
+    status: ProcessStatus
+}
+
+fn create_process(label: &str, command: &str) -> Process {
+    Process { label: label.to_string(), command: command.to_string(), status: ProcessStatus::Halted }
+}
 
 struct State {
     current_selection: usize,
-    processes: Vec<(String, String)>
+    processes: Vec<Process>
 }
 
 impl State {
@@ -30,57 +45,90 @@ impl State {
             self.current_selection -= 1;
         }
     }
+
+    fn start_process(&mut self) {
+        self.processes[self.current_selection].status = ProcessStatus::Running;
+    }
+
+    fn halt_process(&mut self) {
+        self.processes[self.current_selection].status = ProcessStatus::Halted;
+    }
+
+    fn set_halted(&mut self) {
+        self.processes[self.current_selection].status = ProcessStatus::Halting;
+    }
 }
 
 fn main() -> Result<()> {
     let state = State {
         current_selection: 0,
         processes: vec![
-            ("Simple Echo".to_string(), "echo hi".to_string()),
-            ("Echo x10".to_string(), "for i in 1 2 3 4 5 6 7 8 9 10 ; do echo $i; sleep 2 ; done".to_string()),
-            ("vim".to_string(), "vim".to_string())
+            create_process("Simple Echo", "echo hi"),
+            create_process("Echo x10", "for i in 1 2 3 4 5 6 7 8 9 10 ; do echo $i; sleep 2 ; done"),
+            create_process("vim", "vim")
         ]
     };
 
-    enable_raw_mode()?;
-    let mut stdout = stdout();
-    execute!(stdout, cursor::Hide)?;
+    let mut stdout = stdout().into_raw_mode()?;
 
-    draw_screen(&state, &stdout)?;
-    async_std::task::block_on(event_loop(state, &stdout));
+    write!(stdout, "{}", cursor::Hide)?;
 
-    execute!(stdout, cursor::Show)?;
-    disable_raw_mode()?;
+    draw_screen(&state, &stdout).expect("fuck");
+    event_loop(state, &stdout);
+
+    write!(stdout, "{}{}{}", cursor::Goto(0, 1), clear::All, cursor::Show)?;
     Ok(())
 }
 
 fn draw_screen(state: &State, mut stdout: &Stdout) -> Result<()> {
-    const UP: char = '▲';
-    const DOWN: char = '▼';
+    write!(stdout, "{}", clear::All)?;
 
-    queue!(stdout, terminal::Clear(terminal::ClearType::All))?;
+    for (ix, p) in state.processes.iter().enumerate() {
+        match p.status {
+            ProcessStatus::Running =>
+                write!(
+                    stdout,
+                    "{}{} {} ",
+                    cursor::Goto(0, (ix + 1) as u16),
+                    Fg(color::Green),
+                    UP
+                )?,
+            ProcessStatus::Halting =>
+                write!(
+                    stdout,
+                    "{}{} {} ",
+                    cursor::Goto(0, (ix + 1) as u16),
+                    Fg(color::Yellow),
+                    UP
+                )?,
+            ProcessStatus::Halted =>
+                write!(
+                    stdout,
+                    "{}{} {} ",
+                    cursor::Goto(0, (ix + 1) as u16),
+                    Fg(color::Red),
+                    DOWN,
+                )?
+        }
 
-    for (ix, (label, _)) in state.processes.iter().enumerate() {
         if state.current_selection == ix {
-            queue!(
+            write!(
                 stdout,
-                cursor::MoveTo(0, ix as u16),
-                SetForegroundColor(Color::Green),
-                SetBackgroundColor(Color::Blue),
-                Print(format!(" {} ", UP)),
-                SetForegroundColor(Color::Magenta),
-                Print(format!("{:width$}", label, width=30)),
-                ResetColor
+                "{}{}{}{:width$}{}",
+                Bg(color::LightMagenta),
+                Fg(color::Black),
+                style::Bold,
+                p.label,
+                style::Reset,
+                width=20
             )?;
         } else {
-            queue!(
+            write!(
                 stdout,
-                cursor::MoveTo(0, ix as u16),
-                SetForegroundColor(Color::Red),
-                Print(format!(" {} ", DOWN)),
-                SetForegroundColor(Color::Cyan),
-                Print(label),
-                ResetColor
+                "{}{}{}",
+                Fg(color::Cyan),
+                p.label,
+                style::Reset
             )?;
         }
     }
@@ -89,28 +137,38 @@ fn draw_screen(state: &State, mut stdout: &Stdout) -> Result<()> {
     Ok(())
 }
 
-async fn event_loop(mut state: State, stdout: &Stdout) {
-    let mut reader = EventStream::new();
+fn event_loop(mut state: State, mut stdout: &Stdout) {
+    let stdin = stdin();
 
-    loop {
-        match reader.next().await {
-            Some(Ok(event)) => {
-                if event == Event::Key(KeyCode::Char('j').into()) {
-                    state.next_process();
-                    draw_screen(&state, stdout);
-                }
-
-                if event == Event::Key(KeyCode::Char('k').into()) {
-                    state.previous_process();
-                    draw_screen(&state, stdout);
-                }
-
-                if event == Event::Key(KeyCode::Char('q').into()) || event == Event::Key(KeyCode::Esc.into()) {
-                    break;
-                }
-            }
-            Some(Err(e)) => println!("Error: {:?}\r", e),
-            None => break,
+    for c in stdin.keys() {
+        match c {
+            Ok(Key::Char('j')) => {
+                state.next_process();
+                draw_screen(&state, stdout);
+            },
+            Ok(Key::Char('k')) => {
+                state.previous_process();
+                draw_screen(&state, stdout);
+            },
+            Ok(Key::Char('s')) => {
+                state.start_process();
+                draw_screen(&state, stdout);
+            },
+            Ok(Key::Char('x')) => {
+                state.halt_process();
+                draw_screen(&state, stdout);
+            },
+            Ok(Key::Char('h')) => {
+                state.set_halted();
+                draw_screen(&state, stdout);
+            },
+            Ok(Key::Char('q')) => {
+                break;
+            },
+            Err(e) => {
+                write!(stdout, "{}", e);
+            },
+            _ => {}
         }
     }
 }
