b291a6603a3aeb79601ecc2778930b620cf1442a
Eddie Hickey <erhickey@gmail.com>
2023-07-08 12:58:58 -0400

    update

--- src/input.rs
+++ src/input.rs
@@ -17,8 +17,8 @@ fn matches_key(key: Key, acceptable_keys: &[String]) -> bool {
 pub fn input_loop(controller: Controller) -> Result<(), Box<dyn Error>> {
     let keybinding = controller.config.keybinding.clone();
 
-    let a_controller = Arc::new(Mutex::new(controller));
-    a_controller.lock().unwrap().on_startup()?;
+    let am_controller = Arc::new(Mutex::new(controller));
+    am_controller.lock().unwrap().on_startup()?;
 
     let stdin = stdin();
 
@@ -26,36 +26,39 @@ pub fn input_loop(controller: Controller) -> Result<(), Box<dyn Error>> {
         match c {
             Ok(key) => {
                 if matches_key(key, &keybinding.quit) {
-                    a_controller.lock().unwrap().on_keypress_quit()?;
+                    am_controller.lock().unwrap().on_keypress_quit()?;
                     break;
                 } else if matches_key(key, &keybinding.down) {
-                    a_controller.lock().unwrap().on_keypress_down()?;
+                    am_controller.lock().unwrap().on_keypress_down()?;
                 } else if matches_key(key, &keybinding.up) {
-                    a_controller.lock().unwrap().on_keypress_up()?;
+                    am_controller.lock().unwrap().on_keypress_up()?;
                 } else if matches_key(key, &keybinding.start) {
-                    if let Some((pid, process_index)) = a_controller.lock().unwrap().on_keypress_start()? {
-                        let cont = a_controller.clone();
-                        spawn(move || unsafe {
-                            let mut file = std::fs::File::create("foo.txt").unwrap();
-                            use std::io::prelude::*;
-                            let l1 = format!("{}\n", pid);
-                            let _ = file.write_all(l1.as_bytes());
-                            // BUG: waitpid returns immediately, should options be something other than 0?
-                            libc::waitpid(pid, std::ptr::null_mut(), 0);
-                            let _ = file.write_all(b"waitpid done");
-                            let _ = cont.lock().unwrap().on_process_terminated(process_index);
-                        });
+                    if let Some((pid, process_index)) = am_controller.lock().unwrap().on_keypress_start()? {
+                        watch_pid(am_controller.clone(), pid, process_index);
                     }
                 } else if matches_key(key, &keybinding.stop) {
-                    a_controller.lock().unwrap().on_keypress_stop()?;
+                    am_controller.lock().unwrap().on_keypress_stop()?;
                 }
             },
             Err(e) => {
-                a_controller.lock().unwrap().on_error(Box::new(e));
+                am_controller.lock().unwrap().on_error(Box::new(e));
             }
         }
     }
 
-    a_controller.lock().unwrap().on_exit()?;
+    am_controller.lock().unwrap().on_exit()?;
     Ok(())
+}
+
+fn watch_pid(controller: Arc<Mutex<Controller>>, pid: i32, process_index: usize) {
+    spawn(move || unsafe {
+        let mut file = std::fs::File::create("foo.txt").unwrap();
+        use std::io::prelude::*;
+        let l1 = format!("{}\n", pid);
+        let _ = file.write_all(l1.as_bytes());
+        // BUG: waitpid returns immediately, should options be something other than 0?
+        libc::waitpid(pid, std::ptr::null_mut(), 0);
+        let _ = file.write_all(b"waitpid done");
+        let _ = controller.lock().unwrap().on_process_terminated(process_index);
+    });
 }
